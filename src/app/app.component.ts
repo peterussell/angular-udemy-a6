6b3acc4c561770054ebb8945e8c0baca46e719c6
Pete Russell <pinkbatz@gmail.com>
2017-10-11 15:41:42 +1300

    Submission.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  @ViewChild('f') subscForm: NgForm;
+  defaultSubscription: string = "advanced";
+  submitted: boolean = false;
+  subscData = {
+    email: '',
+    subscription: '',
+    password: ''
+  }
 
+  onSubmit() {
+    this.subscData.email = this.subscForm.value.email;
+    this.subscData.subscription = this.subscForm.value.subscription;
+    this.subscData.password = this.subscForm.value.password;
+
+    this.submitted = true;
+    this.subscForm.reset();
+  }
 }
